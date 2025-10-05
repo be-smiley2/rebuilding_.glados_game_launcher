417239c571da5b7d34aeb4752100d1550b521ccf
be-smiley2 <marcusfosberry2@hotmail.com>
2025-10-06 00:50:30 +0100

    Delete glados_launcher directory

--- glados_launcher/__init__.py
+++
@@ -1,5 +0,0 @@
-﻿"""Public package interface for the refactored launcher."""
-from .config import CURRENT_VERSION
-from .gui import ApertureEnrichmentCenterGUI
-
-__all__ = ["ApertureEnrichmentCenterGUI", "CURRENT_VERSION"]
