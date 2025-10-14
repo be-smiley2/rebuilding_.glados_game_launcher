d603a8de63b0f0934c099c3d1bef67ac3a1b8d9b
be-smiley2 <marcusfosberry2@hotmail.com>
2025-10-14 22:23:10 +0100

    Initialize openrouter AI models file

---
+++ aperture science enrichment center game launcher/openrouter ai models.py
@@ -0,0 +1,6 @@
+cognitivecomputations/dolphin-mistral-24b-venice-edition:free
+nvidia/nemotron-nano-9b-v2:free
+deepseek/deepseek-chat-v3.1:free
+meta-llama/llama-3.3-8b-instruct:free
+openai/gpt-oss-20b:free
+qwen/qwen3-4b:free
